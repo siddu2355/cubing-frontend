28c5da0df46feca7ce1868304996cbe7313344d8
siddu2355 <siddusirasanambeti@gmail.com>
2023-10-12 14:24:38 +0530

    changes json config

--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -26,7 +26,7 @@ class Participants extends Component {
         return (
             <div className='App'>
                 <div style={{marginTop:"20px", marginBottom:"20px"}}>
-                  <span className={this.clname("single")}><a href='/results/single'>Single</a></span>
+                  <span className={this.clname("single")}><a aria-disabled="true" href='/results/single'>Single</a></span>
                   <span className={this.clname("average")}><a href='/results/average'>Average</a></span>
                 </div>
                 <Table sa={this.props.sa}/>
